769b7b4a79843366d058be5859a2ce633d2cdf85
Efraim Neto <efraim_tm@hotmail.com>
2024-11-05 17:48:50 -0300

    first commit

---
+++ chatbot/views.py
@@ -0,0 +1,59 @@
+import os
+from django.conf import settings
+from django.http import JsonResponse
+from django.shortcuts import render
+from langchain_groq import ChatGroq
+from markdown import markdown
+from chatbot.models import Chat
+
+# Configura a chave de API para o modelo de linguagem
+os.environ['GROQ_API_KEY'] = settings.GROQ_API_KEY
+
+def get_chat_history(chats):
+    chat_history = []
+    for chat in chats:
+        chat_history.append(
+            ('human', chat.message,)
+        )
+        chat_history.append(
+            ('ai', chat.response,)
+        )
+    return chat_history
+
+def ask_ai(context, message):
+    model = ChatGroq(model='llama-3.2-90b-vision-preview')
+    messages = [
+        (
+            'system',
+            'Você é um assistente responsável por tirar dúvidas sobre programação Python.'
+            'Responda em formato markdown.',
+        ),
+    ]
+    messages.extend(context)
+    messages.append(
+        (
+            'human',
+            message,
+        ),
+    )
+    response = model.invoke(messages)
+    return markdown(response.content, output_format='html')
+
+def chatbot(request):
+    chats = Chat.objects.all()
+    if request.method == 'POST':
+        context = get_chat_history(chats=chats)
+        message = request.POST.get('message')
+        response = ask_ai(context=context, message=message)
+
+        # Cria uma nova entrada de chat com mensagem, resposta e horário de criação
+        chat = Chat(message=message, response=response)
+        chat.save()
+
+        return JsonResponse({
+            'message': message,
+            'response': response,
+            'created_at': chat.created_at.isoformat()  # Envia data completa no formato ISO
+        })
+
+    return render(request, 'chatbot.html', {'chats': chats})
---
+++ core/settings.py
@@ -0,0 +1,126 @@
+import os
+from pathlib import Path
+from decouple import config
+
+GROQ_API_KEY = config('GROQ_API_KEY')
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-09zw1a55baq-z8jnny0ku_%b@b4!-$en%i)&ucnf5@jrrv!)1q'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'chatbot',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'core.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['core/templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'core.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.1/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+
+# Configurações de arquivos estáticos
+STATIC_URL = '/static/'  # URL para acessar arquivos estáticos
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Diretório onde os arquivos estáticos serão armazenados
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Diretório onde os arquivos coletados serão armazenados
+
+# Configurações de arquivos de mídia
+MEDIA_URL = '/media/'  # URL para acessar arquivos de mídia
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Diretório onde os arquivos de mídia serão armazenados
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
